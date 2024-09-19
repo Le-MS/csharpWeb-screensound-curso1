8633b3d90ba78e9b66b3bc717401bef8ad887614
Leticia <l.souza.lms@gmail.com>
2024-09-19 17:09:49 -0300

    ArtistaDal

---
+++ ScreenSound/Banco/ArtistaDAL.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Data.SqlClient;
+using ScreenSound.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScreenSound.Banco
+{
+    internal class ArtistaDAL
+    {
+        public IEnumerable<Artista> Listar()
+        {
+            var lista = new List<Artista>();
+            using var connection = new Connection().obterConexao();
+            connection.Open();
+
+            string sql = "SELECT* FROM Artistas";
+            SqlCommand cmd = new SqlCommand(sql, connection);
+            using SqlDataReader reader = cmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+                string nomeArtista = Convert.ToString(reader["Nome"]);
+                string bioArtista = Convert.ToString(reader["Bio"]);
+                int idArtista = Convert.ToInt32(reader["Id"]);
+
+                Artista artista = new(nomeArtista, bioArtista) { Id = idArtista };
+                lista.Add(artista);
+            }
+            return lista;
+        }
+
+        public void Adicionar(Artista artista)
+        {
+            var lista = new List<Artista>();
+            using var connection = new Connection().obterConexao();
+            connection.Open();
+
+            string sql = "INSERT INTO Artistas (Nome, FotoPerfil, Bio) VALUES (@nome, @perfilPadrao, @bio)";
+            SqlCommand cmd = new SqlCommand(sql, connection);
+
+            cmd.Parameters.AddWithValue("@nome", artista.Nome);
+            cmd.Parameters.AddWithValue("@perfilPadrao", artista.FotoPerfil);
+            cmd.Parameters.AddWithValue("@bio", artista.Bio);
+
+            int retorno = cmd.ExecuteNonQuery();
+            Console.WriteLine($"Linhas afetadas: {retorno}");
+        }
+    }
+}
---
+++ ScreenSound/Banco/Connection.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Data.SqlClient;
+using ScreenSound.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace ScreenSound.Banco
+{
+    internal class Connection
+    {
+        private string connectionString = $@"Data Source=DESKTOP-U6VMJ2C\SQLSERVER2022;Initial Catalog=Alura_Aula_Csharp;Integrated Security=True;Encrypt=True;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False";
+
+        public SqlConnection obterConexao()
+        {
+            return new SqlConnection(connectionString);
+        }
+
+       
+    }
+}
